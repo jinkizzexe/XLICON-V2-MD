b02d02df63f877e5511a68754e15ef5e1de3f1dd
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-26 12:56:10 +0100

    Update cmd-set.js

--- plugins/cmd-set.js
+++ plugins/cmd-set.js
@@ -7,7 +7,7 @@ let handler = async (m, { text, usedPrefix, command }) => {
     if (!text) throw `✳️ command is missing`
     let sticker = global.db.data.sticker
     let hash = m.quoted.fileSha256.toString('base64')
-    if (sticker[hash] && sticker[hash].locked) throw '⚠️ You do not have permission to change this Sticker command'
+    if (sticker[hash] && sticker[hash].locked) throw '⚠️ ليس لك الحق في تغيير اب شئ'
     sticker[hash] = {
         text,
         mentionedJid: m.mentionedJid,
