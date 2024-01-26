9ae814e88e2b42b1f85c27ac0c6e5c4975aef088
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-26 12:38:40 +0100

    Update GURU-qrmaker.js

--- plugins/GURU-qrmaker.js
+++ plugins/GURU-qrmaker.js
@@ -1,9 +1,9 @@
 import { toDataURL } from 'qrcode'
 let handler = async (m, { text, conn }) => {
-if (!text) throw `*Give a text to convert*`
-conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', 'Here u go', m)
+if (!text) throw `*قم بكتابة النص التي تريد تخويله الى QR ...\n instagram \n https://www.instagram.com/ovmar_1 \n Whatsapp GP \n https://chat.whatsapp.com/EtvWb5preas43EOZNqALjx*`
+conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', 'هاذا هو رمز QR الخاص بك .. \n instagram \n https://www.instagram.com/ovmar_1 \n Whatsapp GP \n https://chat.whatsapp.com/EtvWb5preas43EOZNqALjx, m)
 }
-handler.help = ['', 'code'].map(v => 'qr' + v + ' <text>')
+handler.help = ['', 'code'].map(v => 'qr' + v + ' <LEZWA>')
 handler.tags = ['tools']
 handler.command = /^qr(code)?$/i
 export default handler
