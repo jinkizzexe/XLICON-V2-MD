4a00bebdcda69a0355460839fa6246db6dc5c170
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-26 13:00:50 +0100

    Update dl-igstalk.js

--- plugins/dl-igstalk.js
+++ plugins/dl-igstalk.js
@@ -2,18 +2,18 @@
 import fg from 'api-dylux'
 let handler= async (m, { conn, args, text, usedPrefix, command }) => {
 	
-    if (!args[0]) throw `✳️ Enter the Instagram Username\n\n📌Example: ${usedPrefix + command} asli_guru69` 
+    if (!args[0]) throw `قم باإرسال إسم الشخص..\n\n📌مثال: ${usedPrefix + command} Ovmar_1` 
     let res = await fg.igStalk(args[0])
     let te = `
 ┌──「 *STALKING* 
-▢ *🔖Number:* ${res.name} 
-▢ *🔖Username:* ${res.username}
-▢ *👥followers:* ${res.followersH}
-▢ *🫂following:* ${res.followingH}
-▢ *📌Bio:* ${res.description}
-▢ *🏝️Posts:* ${res.postsH}
+▢ *🔖رقم:* ${res.name} 
+▢ *🔖إسم الحساب:* ${res.username}
+▢ *👥يتابعه:* ${res.followersH}
+▢ *🫂يتابع:* ${res.followingH}
+▢ *📌سيرة:* ${res.description}
+▢ *🏝️منشورات:* ${res.postsH}
 
-▢ *🔗 Link* : https://instagram.com/${res.username.replace(/^@/, '')}
+▢ *🔗 رابط* : https://instagram.com/${res.username.replace(/^@/, '')}
 └────────────`
 
      await conn.sendFile(m.chat, res.profilePic, 'tt.png', te, m)
