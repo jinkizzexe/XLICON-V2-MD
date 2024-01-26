622e199bf4a57debf0dd40bc46784a1dafd608d5
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-26 12:39:50 +0100

    Update GURU-readqr.js

--- plugins/GURU-readqr.js
+++ plugins/GURU-readqr.js
@@ -3,11 +3,11 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '*Respond to a qr*'
+if (!mime) throw '*قم بالرد على Qr التي تريد ان تعرف مافي داخله .. \n instagram \n https://www.instagram.com/ovmar_1 \n Whatsapp GP \n https://chat.whatsapp.com/EtvWb5preas43EOZNqALjx*'
 let img = await q.download?.()
 let url = await uploadImage(img)
 let anu = await fetch(`https://api.lolhuman.xyz/api/read-qr?apikey=${lolkeysapi}&img=${url}`)
 let json = await anu.json()
-await m.reply(`*here u go:* ${json.result}`)}
+await m.reply(`*هاذا هو النص الذي داخل Qr .. \n instagram \n https://www.instagram.com/ovmar_1 \n Whatsapp GP \n https://chat.whatsapp.com/EtvWb5preas43EOZNqALjx:* ${json.result}`)}
 handler.command = /^(readqr)$/i
 export default handler
